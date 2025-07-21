70d3955ca9909fcb0d1e024d99fc1e6821019d50
QingFeng-awa <qingfeng_awa@163.com>
2025-07-21 19:10:46 +0800

    大幅调整文本、下载私服新增源选择

--- Tool.bat
+++ Tool.bat
@@ -1,11 +1,11 @@
 chcp 65001 > nul
 @echo off
-set "Version=2.0.1"
-title QingFeng AmongUs工具箱
+set "Version=2.1.0"
+title 清风 Among Us 工具箱
 echo.
 echo ======================================================
 echo.
-echo QingFeng AmongUs工具箱
+echo 清风 Among Us 工具箱
 echo.
 echo 版本：%Version%
 echo.
@@ -19,7 +19,7 @@ cls
 echo.
 echo ======================================================
 echo.
-echo QingFeng AmongUs工具箱
+echo 清风 Among Us 工具箱
 echo.
 echo 正在初始化脚本……
 echo.
@@ -33,7 +33,7 @@ cls
 echo.
 echo ======================================================
 echo.
-echo QingFeng AmongUs工具箱
+echo 清风 Among Us 工具箱
 echo.
 echo 系统版本检查……
 echo.
@@ -45,13 +45,13 @@ cls
 echo.
 echo ======================================================
 echo.
-echo QingFeng AmongUs工具箱
+echo 清风 Among Us 工具箱
 echo.
-echo 你的Windows版本已过时，请升级你的Windows。
+echo 你的 Windows 版本已过时，请更新你的 Windows。
 echo.
 echo ======================================================
 echo.
-set "InitializationErrorMessage=需要Windows 10/11才能运行此脚本。"
+set "InitializationErrorMessage=需要 Windows 10/11 才能运行此脚本。"
 timeout /t 2 /NoBreak > nul
 goto initialization_failed
 
@@ -60,7 +60,7 @@ cls
 echo.
 echo ======================================================
 echo.
-echo QingFeng AmongUs工具箱
+echo 清风 Among Us 工具箱
 echo.
 echo 依赖项检查……
 echo.
@@ -72,13 +72,13 @@ where curl > nul 2>&1 || (
   echo.
   echo ======================================================
   echo.
-  echo QingFeng AmongUs工具箱
+  echo 清风 Among Us 工具箱
   echo.
-  echo 你的电脑缺失了必备组件,请自行安装。
+  echo 你的电脑缺失了必备组件，请自行安装。
   echo.
   echo ======================================================
   echo.
-  set "InitializationErrorMessage=缺失组件curl，需要此组件才能运行工具箱的完整功能。"
+  set "InitializationErrorMessage=缺失组件 cURL，需要此组件才能运行工具箱的完整功能。"
   timeout /t 2 /NoBreak > nul
   goto initialization_failed
 )
@@ -92,7 +92,7 @@ echo 工具箱主菜单
 echo.
 echo 1. 安装清风服
 echo.
-echo 2. 修复旧版AmongUs黑屏问题
+echo 2. 修复旧版 Among Us 黑屏问题
 echo.
 echo 3. 关于此工具箱
 echo.
@@ -101,7 +101,7 @@ echo.
 echo ======================================================
 echo.
 timeout /t 1 /NoBreak > nul
-choice /c 1234 /n /m "请输入你的选择：[1,2,3,4]"
+choice /c 1234 /n /m "请输入你的选择：[1，2，3，4]"
 if "%ERRORLEVEL%"=="0" goto main_menu
 if "%ERRORLEVEL%"=="1" goto install_server_wait
 if "%ERRORLEVEL%"=="2" goto fix_old_amongus_wait
@@ -136,7 +136,7 @@ echo.
 echo ======================================================
 echo.
 timeout /t 1 /NoBreak > nul
-choice /n /m "你确定要继续此操作吗?输入y确认,n取消并返回主菜单。"
+choice /n /m "你确定要继续此操作吗？输入 Y 确认，输入 N 取消并返回主菜单。"
 if "%ERRORLEVEL%"=="0" goto install_server_confirm
 if "%ERRORLEVEL%"=="1" goto install_server_delete
 if "%ERRORLEVEL%"=="2" goto main_menu
@@ -154,7 +154,30 @@ echo ======================================================
 echo.
 timeout /t 2 /NoBreak > nul
 del /f "%APPDATA%\..\LocalLow\Innersloth\Among Us\regionInfo.json" > nul 2>&1
-goto install_server_download
+goto install_server_region_choose
+
+:install_server_region_choose
+cls
+echo.
+echo ======================================================
+echo.
+echo 安装清风服
+echo.
+echo 选择下载源
+echo.
+echo 1. Github
+echo.
+echo 2. FengCloud(服务维护，暂不可用)
+echo.
+echo ======================================================
+echo.
+timeout /t 1 /NoBreak > nul
+choice /n /m "请输入你的选择[1]："
+if "%ERRORLEVEL%"=="0" goto install_server_region_choose
+if "%ERRORLEVEL%"=="1" (
+  set "ServerDownloadURL=https://raw.githubusercontent.com/QingFengTechnology/FengAmongUsTool/refs/heads/main/regionInfo.json"
+  goto install_server_download
+)
 
 :install_server_download
 cls
@@ -168,18 +191,12 @@ echo.
 echo ======================================================
 echo.
 timeout /t 2 /NoBreak > nul
-curl --ssl-no-revoke "https://api.xtreme.net.cn/Server/regionInfo.json" -o "%APPDATA%\..\LocalLow\Innersloth\Among Us\regionInfo.json"
+curl --ssl-no-revoke "%ServerDownloadURL%" -o "%APPDATA%\..\LocalLow\Innersloth\Among Us\regionInfo.json"
 if %ERRORLEVEL% neq 0 (
-  cls
-  echo.
-  echo ======================================================
-  echo.
-  echo 你无法安装服务器配置文件。
   echo.
   echo 下载文件出现时异常，下载失败。
   echo.
-  echo ======================================================
-  echo.
+  echo 如果脚本无问题，那么你应该能看到错误信息，请使用电脑软件截图并发送至天云群以请求帮助。
   timeout /t 1 /NoBreak > nul
   echo 按任意键返回主菜单。
   echo.
@@ -194,15 +211,15 @@ cls
 echo.
 echo ======================================================
 echo.
-echo 服务器配置文件安装完成。
+echo 服务器安装完成。
 echo.
-echo 要立即打开AmongUs吗?
+echo 要立即打开 Among Us 吗?
 echo.
-echo 此操作通过Steam进行,因而有些情况下AmongUs启动可能不是那么及时。
+echo 此操作通过 Steam 进行，因而有些情况下 Among Us 启动可能不是那么及时。
 echo.
 echo ======================================================
 echo.
-choice /n /m "输入y确认并返回主菜单,输入n直接返回主菜单。"
+choice /n /m "输入 Y 确认并返回主菜单，输入 N 直接返回主菜单。"
 if "%ERRORLEVEL%"=="0" goto install_server_done
 if "%ERRORLEVEL%"=="1" goto install_server_run_amongus
 if "%ERRORLEVEL%"=="2" goto main_menu
@@ -214,7 +231,7 @@ echo ======================================================
 echo.
 echo 安装清风服
 echo.
-echo 正在打开AmongUs……
+echo 正在打开 Among Us……
 echo.
 echo ======================================================
 echo.
@@ -227,15 +244,17 @@ cls
 echo.
 echo ======================================================
 echo.
-echo 关于QingFeng AmongUs工具箱
+echo 关于清风 Among Us 工具箱
 echo.
 echo 版本: %Version%
 echo.
 echo 开发者: QingFeng (https://github.com/QingFeng-awa)
 echo.
-echo 工具箱仍在持续维护中,因此可能有功能还尚未开发,或存在bug。
+echo 仓库链接：https://github.com/QingFengTechnology/FengAmongUsTool
 echo.
-echo 如果你遇到了bug或是想提议一个新功能,请联系QingFeng。
+echo 工具箱仍在持续维护中，因此可能有功能还尚未开发，或存在 Bug。
+echo.
+echo 如果你遇到了 Bug 或是想提议一个新功能，请提出 issue。
 echo.
 echo ======================================================
 echo.
@@ -250,7 +269,7 @@ cls
 echo.
 echo ======================================================
 echo.
-echo 修复旧版AmongUs黑屏问题
+echo 修复旧版 Among Us 黑屏问题
 echo.
 echo 请稍候……
 echo.
@@ -264,14 +283,14 @@ cls
 echo.
 echo ======================================================
 echo.
-echo 修复旧版AmongUs黑屏问题
+echo 修复旧版 Among Us 黑屏问题
 echo.
-echo 此操作通过替换或删除文件来修复旧版AmongUs黑屏问题。
+echo 此操作通过替换或删除文件来修复旧版 Among Us 黑屏问题。
 echo.
 echo ======================================================
 echo.
 timeout /t 1 /NoBreak > nul
-choice /n /m "请自行备份相关文件,输入y继续执行,输入n返回主菜单。"
+choice /n /m "请自行备份相关文件，输入 Y 继续执行，输入 N 返回主菜单。"
 if "%ERRORLEVEL%"=="0" goto fix_old_amongus_confirm
 if "%ERRORLEVEL%"=="1" goto fix_old_amongus_choose
 if "%ERRORLEVEL%"=="2" goto main_menu
@@ -283,14 +302,16 @@ echo ======================================================
 echo.
 echo 选择修复方式
 echo.
-echo 1. 直接删除 (AmongUs会自动生成默认配置文件,但这会导致包括语言选项的自定义配置全部丢失。)
+echo 1. 直接删除 (AmongUs会自动生成默认配置文件，但这会导致包括语言选项的自定义配置全部丢失。)
 echo.
-echo 2. 下载文件替换 (需要网络连接,且下载文件只是帮你设定了语言为简体中文,其余设置均为默认。)
+echo 2. 下载文件替换 (需要网络连接，且下载文件只是帮你设定了语言为简体中文，其余设置均为默认。)
+echo.
+echo 上述两种方式都可能导致极个别模组储存的存档丢失，请自行备份文件。
 echo.
 echo ======================================================
 echo.
 timeout /t 1 /NoBreak > nul
-choice /c 12 /n /m "请选择修复方式：[1,2]"
+choice /c 12 /n /m "请选择修复方式：[1，2]"
 if "%ERRORLEVEL%"=="0" goto fix_old_amongus_choose
 if "%ERRORLEVEL%"=="1" goto fix_old_amongus_delete
 if "%ERRORLEVEL%"=="2" goto fix_old_amongus_download
@@ -300,7 +321,7 @@ cls
 echo.
 echo ======================================================
 echo.
-echo 修复旧版AmongUs黑屏问题
+echo 修复旧版 Among Us黑屏问题
 echo.
 echo 正在删除配置文件……
 echo.
@@ -315,7 +336,7 @@ cls
 echo.
 echo ======================================================
 echo.
-echo 修复旧版AmongUs黑屏问题
+echo 修复旧版 Among Us 黑屏问题
 echo.
 echo 正在下载配置文件……
 echo.
@@ -368,7 +389,7 @@ echo 脚本版本: %Version%
 echo.
 echo 错误原因：%InitializationErrorMessage%
 echo.
-echo 如果你无法理解这是什么，请将此段错误截图发送给(天云群)群成员以寻求帮助。
+echo 如果你无法理解这是什么，请使用电脑截图错误信息并发送给天云群以寻求帮助。
 echo.
 echo ======================================================
 echo.
