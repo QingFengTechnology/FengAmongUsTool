7faf085a91ab45353c6943725cada09c5b83540a
QingFeng-awa <qingfeng_awa@163.com>
2025-10-05 19:20:12 +0800

    更新版本号

--- app/function/variable.py
+++ app/function/variable.py
@@ -1,6 +1,6 @@
-Version = "v3.0.0-pre.2"
-VersionType = "preview"
-versionDate = "2025-08-28T01:29"
+Version = "v3.0.0"
+VersionType = "release"
+versionDate = "2025-10-05T00:00"
 
 UpdateAvailable = False
 UpdateInfo = None
