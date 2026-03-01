50459b667502be369cca237652b576d8b3c6a804
QingFeng-awa <qingfeng_awa@163.com>
2026-03-02 02:18:22 +0800

    优化开发版本提示

--- app/function/main.py
+++ app/function/main.py
@@ -4,7 +4,7 @@ from rich.panel import Panel
 from rich.box import DOUBLE_EDGE
 from rich.console import Console
 
-from function.variable import Version, ToolTitle
+from function.variable import Version, VersionType, ToolTitle
 from function.updateCheck import updateNotification
 
 console = Console()
@@ -13,14 +13,14 @@ def cls():
   """仿制批处理的 cls 清屏操作"""
   print("\033c", end="")
 
-def defaultHeader(title=ToolTitle,version=Version):
+def defaultHeader(title=ToolTitle, version=Version):
     """打印默认工具箱标题"""
     cls()
     console.print(Panel(Text(title, style="bold", justify="left"), subtitle=version, box=DOUBLE_EDGE))
     # alpha 版本检查
-    if Version.find("alpha") != -1:
+    if VersionType == "alpha" or VersionType == "beta":
         br()
-        console.print(Panel(Text("\n当前版本为[orange1]开发版本[/orange1]，可能存在较多问题。\n", style="bold", justify="center"),title="警告"))
+        console.print(Panel(Text("\n当前版本为开发版本，可能存在较多问题。\n", justify="center"), title="警告", style="yellow1"))
     # 显示更新提示（如果有）
     import function.variable
     if function.variable.UpdateAvailable and function.variable.UpdateInfo:
@@ -41,7 +41,7 @@ def defaultHeader(title=ToolTitle,version=Version):
     screenWidth = ctypes.windll.user32.GetSystemMetrics(0)
     if windowWidth <= screenWidth:
         br()
-        console.print(Panel(Text("\n当前窗口似乎并未最大化显示，这可能会影响显示效果。\n", justify="center"),title="警告", style="yellow1"))
+        console.print(Panel(Text("\n当前窗口似乎并未最大化显示，这可能会影响显示效果。\n", justify="center"), title="警告", style="yellow1"))
 
 def br():
   """(名称)HTML 风格的换行"""
--- app/function/variable.py
+++ app/function/variable.py
@@ -1,7 +1,8 @@
 from datetime import date
+from typing import Literal
 
 Version: str = "v3.3.0LTS"
-VersionType: str = "release"
+VersionType: Literal["alpha", "beta", "preview", "release"] = "release"
 versionDate: date = date(2026, 3, 1)
 
 UpdateAvailable: bool = False
